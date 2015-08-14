77de0f2563b23330daee0b89b3c28861251ef06f
Stephen Ribbon <stribbon@icloud.com>
2015-08-14 15:27:38 -0700

    sorting works, add comments form and delete

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.controller('PostCtrl', function($scope, PostService){
   $scope.addPost = function (post){
   	post.id = $scope.posts.length;
   	post.date = new Date();
+  	post.votes = 0;
   	postToPush = {
   		id: post.id,
   		title: post.title,
@@ -32,6 +33,22 @@ app.controller('PostCtrl', function($scope, PostService){
   	var index = $scope.posts.indexOf(post);
   	$scope.posts.splice(index,1);
   	$scope.removed = 'Post successfully removed.';
+  };
+
+  $scope.addVote = function (post){
+  	post.votes += 1;
+  }
+  $scope.subVote = function (post){
+  	post.votes -= 1;
+  }
+  $scope.sort = "-votes";
+  $scope.setSort = function(type){
+  	if(type == 'votes'){
+  		$scope.sort = '-votes'
+  	} else {
+
+  	$scope.sort = type;
+  	}  
   };
 });
 
@@ -79,9 +96,9 @@ app.factory("PostService", [function () {
   }
 
   var PostList = [
-    {id: 0, title: 'Malibu', author: 'Damon', date: new Date(), img: "http://hdontap.com/images/uploads/gallery_images/63/breaking-point-malibu-631.jpg", body:'I was in Malibu, blazin up with the homies. Dippin in the cool ocean, sipping wine & beers.', comments: [{author:"Casey", body: "That was chill"}, {author:"Stephen", body: "Those fish tacos were bomb"}] },
-    {id: 1, title: 'Sedona', author: 'Stephen', date: new Date(), img: "http://phoenixsedonadaytrip.com/wp-content/uploads/2010/09/Sedona-2-small1.jpg", body:'Solo trip. I need some fresh landscapes', comments: [{author:"Jean-Bastiste", body: "A la prochaine"}, {author:"Stephen", body: "Thanks JB, bon voyage"}] },
-    {id: 2, title: 'Vegas', author: 'Casey', date: new Date(), img: "http://addictedtocostco.com/wp-content/uploads/2015/03/20150317-1.jpg", body:'I was here for the Blackhat Hacker Conference', comments: [{author:"Casey", body: "Damn I wish I could just been alone!"}, {author:"Stephen", body: "Woops, I probably shouldn't of come"}] },  
+    {id: 0, title: 'Malibu', author: 'Damon', date: new Date(), img: "http://hdontap.com/images/uploads/gallery_images/63/breaking-point-malibu-631.jpg", body:'I was in Malibu, blazin up with the homies. Dippin in the cool ocean, sipping wine & beers.', votes: 1, comments: [{author:"Casey", body: "That was chill"}, {author:"Stephen", body: "Those fish tacos were bomb"}] },
+    {id: 1, title: 'Sedona', author: 'Stephen', date: new Date(), img: "http://phoenixsedonadaytrip.com/wp-content/uploads/2010/09/Sedona-2-small1.jpg", body:'Solo trip. I need some fresh landscapes', votes: 3, comments: [{author:"Jean-Bastiste", body: "A la prochaine"}, {author:"Stephen", body: "Thanks JB, bon voyage"}] },
+    {id: 2, title: 'Vegas', author: 'Casey', date: new Date(), img: "http://addictedtocostco.com/wp-content/uploads/2015/03/20150317-1.jpg", body:'I was here for the Blackhat Hacker Conference', votes: -2, comments: [{author:"Casey", body: "Damn I wish I could just been alone!"}, {author:"Stephen", body: "Woops, I probably shouldn't of come"}] },  
   ];
 
   return factory;
